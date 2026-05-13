d9b4b6ae04d6ec9a48f7b9ab0203ac468f44a321
julcreator <julcreator@users.noreply.github.com>
2026-05-13 21:28:58 +0200

    remove Booking and Ticketmaster affiliate links

--- push.bat
+++ push.bat
@@ -1,5 +1,5 @@
 @echo off
 cd C:\Users\Usuario\projects\ibizacoolparty
 git add -A
-git commit -m "add Pexels gallery and analytics fix"
+git commit -m "remove Booking and Ticketmaster affiliate links"
 git push origin master
